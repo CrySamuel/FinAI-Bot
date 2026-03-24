b3e72fe32f5d9df8d08760d6e8ea9e6630565121
Crystofer Samuel <127315536+CrySamuel@users.noreply.github.com>
2026-03-24 20:20:27 -0300

    feat: migra banco de dados para PostgreSQL na nuvem

--- src/database/database.py
+++ src/database/database.py
@@ -1,15 +1,19 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-engine = create_engine('sqlite:///financas.db', connect_args={'check_same_thread': False})
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./financeiro.db")
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
